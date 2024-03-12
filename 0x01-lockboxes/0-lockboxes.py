6574c50b4d2690bcde3612a811b028c1ec13e53b
HazelDaniel <hazeldaniel856@gmail.com>
2024-03-12 21:24:07 +0100

    documented the module and functions

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
+"""this module provides a solution to the lockbox problem"""
 from collections import deque
 
 
 class Queue:
     """a class implementation of a queue"""
     def __init__(self):
+        """the constructor method of the class"""
         self.items = deque()
 
     def enqueue(self, item):
+        """this method enqueues items to the queue"""
         self.items.append(item)
 
     def dequeue(self):
+        """this method dequeue items from the queue"""
         if not self.is_empty():
             return self.items.popleft()
         else:
             raise IndexError("Dequeue from empty queue")
 
     def is_empty(self):
+        """this method checks if the queue is empty"""
         return len(self.items) == 0
 
     @property
     def size(self):
+        """this getter method returns the size of the queue"""
         return len(self.items)
 
     def __str__(self):
+        """this returns the string value of the class"""
         return ", ".join([f"{x}" for x in self.items])
 
 
